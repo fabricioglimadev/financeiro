14522ba732fd0a538a831b9033835be06a0a16b0
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-07-19 20:32:28 -0300

    Primeiro commit

---
+++ src/test/java/br/dev/fabricioglima/financeiro/FinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.fabricioglima.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
