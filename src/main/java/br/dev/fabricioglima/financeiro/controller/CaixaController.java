6d1cc2eb71da1f004e4e005b1bbd9c1b90759e77
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-07-19 21:22:16 -0300

    Classe CaixaController método post insert

--- src/main/java/br/dev/fabricioglima/financeiro/controller/CaixaController.java
+++ src/main/java/br/dev/fabricioglima/financeiro/controller/CaixaController.java
@@ -1,16 +1,37 @@
 package br.dev.fabricioglima.financeiro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.dev.fabricioglima.financeiro.dtos.CaixaRequestDto;
+import br.dev.fabricioglima.financeiro.dtos.CaixaResponseDto;
+import br.dev.fabricioglima.financeiro.service.CaixaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/caixa")
 public class CaixaController {
 
+  private final CaixaService service;
+
+  public CaixaController(CaixaService service) {
+    this.service = service;
+  }
+
+
   @GetMapping("/healthcheck")
   public String healthCheck(){
     return "API está no ar";
+  }
+
+  @PostMapping
+  public ResponseEntity<?> insert(@RequestBody CaixaRequestDto request){
+
+    try{
+      CaixaResponseDto response = service.insert(request);
+      return ResponseEntity.ok(response);
+    } catch (Exception e){
+      return ResponseEntity.internalServerError().build();
+    }
+
   }
 
 
