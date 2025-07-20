db81cfd1ac031568f2e1cddd5bb85d8a55a61c5e
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-07-19 21:05:44 -0300

    Classe CaixaService método insert

---
+++ src/main/java/br/dev/fabricioglima/financeiro/service/CaixaService.java
@@ -0,0 +1,45 @@
+package br.dev.fabricioglima.financeiro.service;
+
+import br.dev.fabricioglima.financeiro.CaixaRepository;
+import br.dev.fabricioglima.financeiro.dtos.CaixaRequestDto;
+import br.dev.fabricioglima.financeiro.dtos.CaixaResponseDto;
+import br.dev.fabricioglima.financeiro.entity.CaixaEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class CaixaService {
+
+  private final CaixaRepository repository;
+
+  public CaixaService(CaixaRepository repository) {
+    this.repository = repository;
+  }
+
+
+  public CaixaResponseDto insert(CaixaRequestDto request) {
+
+    CaixaEntity caixaEntity = new CaixaEntity();
+    caixaEntity.setTipoLancamento(request.getTipoLancamento());
+    caixaEntity.setDataLancamento(LocalDate.now());
+    if (request.getDataLancamento() != null) {
+      caixaEntity.setDataLancamento(request.getDataLancamento());
+    }
+    caixaEntity.setDescricao(request.getDescricao());
+    caixaEntity.setValor(request.getValor());
+
+    caixaEntity = repository.save(caixaEntity);
+
+    CaixaResponseDto response = new CaixaResponseDto();
+    response.setTipoLancamento(caixaEntity.getTipoLancamento());
+    response.setDataLancamento(caixaEntity.getDataLancamento());
+    response.setDescricao(caixaEntity.getDescricao());
+    response.setValor(caixaEntity.getValor());
+    response.setId(caixaEntity.getId());
+
+    return response;
+  }
+
+
+}
